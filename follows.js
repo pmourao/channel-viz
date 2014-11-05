0901333708dad1927c302b1757f34feacd5cd84e
pmourao <mourao89@gmail.com>
2014-11-05 20:28:38 +0000

    Update follows.js
    changed default key and feeds

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= '7XLNSmM5IonCUhX7XnJ1baso3bawIOryWCPvXNvdD6g', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1347017346], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Wine Monitoring', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
